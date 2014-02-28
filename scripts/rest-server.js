201556aff6d6ef8738f5db9691bb3756eede7b50
Tom Abbott <tom@stormpath.com>
2014-02-27 16:03:05 -0800

    Hollar

--- scripts/rest-server.js
+++ scripts/rest-server.js
@@ -1,29 +1,46 @@
 var restify = require('restify'),
 	util = require('util')
-	stormpath = require('./stormpath/lib/stormpath');
+	stormpath = require('./stormpath/lib/stormpath'),
+	Application = require('./stormpath/lib/application');
 
 var apiKey = {id: '34USPVUATE8KX180ICLUT03CO', 
-			  secret: 'PHz3f+g33b4ZGsTwtKNCht78Az3iJ7pY20DJ97Dv/X8'},
-	client = null, 
-	server = null;
+			  secret: 'PHz3f+g33b4ZGsTwtKNCht78Az3iJ7pY20DJ97Dv/X8'};
 
+appId = '5jaB29lf9fanIBTywhEIne';
 
 //Only create a server if we can get the current tenant
 client = stormpath.createClient({apiKey: apiKey});
 
 client.getCurrentTenant(function (err, tenant) {
  	if (err) throw err;
+
  	server = restify.createServer();
 
- 	server.get('/hello/:name', handleRequest);
- 	server.head('/hello/:name', handleRequest);
+ 	var handleRequest = function (req, res, next){
+ 		href = tenant.href + "/applications/"+ appId;
+		tenant.dataStore.getResource("https://api.stormpath.com/v1/applications/5jaB29lf9fanIBTywhEIne", null, Application, function(err, obj){
+			if (err) throw err;
+			obj.login("tom@abbott.com", "TomAbbott1", function (err, obj){
+				console.log(err, obj);
+			});
+		});
+	};
 
- 	server.listen(8080, function() {
+ 	server.get('/login/', handleRequest);
+
+ 	server.get(/.*/, restify.serveStatic({
+  		directory: './app/',
+  		default: 'index.html'
+	}));
+	server.get(/.*/, restify.serveStatic({
+  		directory: './bower_components/',
+  		default: 'index.html'
+	}));
+ 	server.listen(8080, function() { 
   		console.log('%s listening at %s', server.name, server.url);
 	});
+
 });
 
 
-var handleRequest = function (req, res, next){
-	res.send('hello ' + req.params.name);
-};
+
---
+++ scripts/stormpath/lib/application.js
@@ -0,0 +1,21 @@
+'use strict';
+var util = require('util');
+var InstanceResource = require('./resource').InstanceResource;
+
+function Application(){
+	Application.super_.apply(this, arguments);
+}
+
+util.inherits(Application, InstanceResource);
+
+Application.prototype.login = function login(username, password, callback){
+	var _this = this;
+	console.log(username, password);
+	var base64uidpassword = new Buffer(username+":"+password).toString('base64');
+	this.dataStore.createResource(_this.loginAttempts.href, {
+		type: 'basic',
+		value: base64uidpassword
+	}, callback);
+}
+
+module.exports = Application;
