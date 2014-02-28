4b22704f1b1c3eebb61db2d34285068acb82c9ba
Jeff Wysong <jeff@stormpath.com>
2014-02-27 19:14:09 -0500

    Updated login page

--- app/js/app.js
+++ app/js/app.js
@@ -15,6 +15,6 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl3'});
-  $routeProvider.when('/login', {templateUrl: 'partials/partial4.html', controller: 'MyCtrl3'});
+  $routeProvider.when('/login', {templateUrl: 'partials/partial4.html', controller: 'LoginCtrl'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
